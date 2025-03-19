680014aac0198918a3246eb5637e421d9da71b41
uzukidd <uzukidd@gmail.com>
2025-03-19 15:18:52 +0800

    feat: rooftop added.

--- vehicle_reconstruction/scene_segmentation/loss_utils.py
+++ vehicle_reconstruction/scene_segmentation/loss_utils.py
@@ -7,16 +7,15 @@ from vehicle_reconstruction import *
 from cudaext.ops.trilinear_interpolate.trilinear_interpolate_utils import Trilinear_interpolation_cuda
 
 class shape_regular(nn.Module):
-    def __init__(self, eigens: torch.Tensor) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.eigens = eigens.detach().clone().view(1, -1)  # [K]
 
-    def forward(self, latent: torch.Tensor):
+    def forward(self, latent: torch.Tensor, eigens:torch.Tensor):
         """
-            latent: [1, K]
+            latent: [B, K]
+            eigens: [K]
         """
-        assert latent.size(0) == 1, "batch forwarding is not yet supported."
-        return 0.5 * (latent/self.eigens).square().sum()
+        return 0.5 * (latent/eigens).square().mean()
     
 class vehicles_roi_pooling(nn.Module):
     
@@ -115,89 +114,3 @@ class pose_estimate_loss_batch(nn.Module):
         loss = F.huber_loss(sdf_val, torch.zeros_like(sdf_val))
 
         return loss
-
-class pose_estimate_loss(nn.Module):
-    """
-        loss from https://www.vision.rwth-aachen.de/media/papers/EngelmannGCPR16_SZa4QgP.pdf
-    """
-
-    def __init__(self, length: int,
-                 width: int,
-                 height: int,
-                 grid_res: float = 0.1) -> None:
-
-        super().__init__()
-        self.length = length
-        self.width = width
-        self.height = height
-        self.grid_res = grid_res
-        self.trilinear_interpolate = Trilinear_interpolation_cuda()
-
-    def forward(self, voxels: torch.Tensor, pts_centroid: torch.Tensor, height_gt: int):
-        """
-            voxels: [B, L, W, H]
-            pts_centroid: [B, N, 3]
-            height: [B, N, 1]
-            ////////////
-            voxels: [L, W, H]
-            pts_centroid: [N, 3]
-            height: int
-        """
-        assert pts_centroid.size().__len__() < 3, "batch forwarding is not yet supported."
-
-        grid_l, grid_w, grid_h = voxels.size(0), voxels.size(1), voxels.size(2)
-
-        # center position
-        pts_centroid[:, 0] += (self.length/2.0)
-        pts_centroid[:, 1] += (self.width/2.0)
-        pts_centroid[:, 2] += (height_gt/2.0)
-
-        # compute grid position
-        with torch.no_grad():
-            x_min = torch.floor(
-                pts_centroid[:, 0] / self.grid_res)
-            x_max = x_min + 1
-
-            y_min = torch.floor(
-                pts_centroid[:, 1] / self.grid_res)
-            y_max = y_min + 1
-
-            z_min = torch.floor(
-                pts_centroid[:, 2] / self.grid_res)
-            z_max = z_min + 1
-
-        pts_centroid[:, 0] -= (x_min * self.grid_res)
-        pts_centroid[:, 1] -= (y_min * self.grid_res)
-        pts_centroid[:, 2] -= (z_min * self.grid_res)
-
-        # project to [-1.0, 1.0]
-        pts_centroid[:, :] *= (2 / self.grid_res)
-        pts_centroid[:, :] -= 1.0
-
-        # limit x, y, z into grid boundary
-        x_min = x_min.clamp_(0, grid_l-1).long()
-        y_min = y_min.clamp_(0, grid_w-1).long()
-        z_min = z_min.clamp_(0, grid_h-1).long()
-        x_max = x_max.clamp_(0, grid_l-1).long()
-        y_max = y_max.clamp_(0, grid_w-1).long()
-        z_max = z_max.clamp_(0, grid_h-1).long()
-
-        feature_stack = torch.stack([
-            voxels[x_max, y_max, z_max],
-            voxels[x_max, y_max, z_min],
-            voxels[x_max, y_min, z_max],
-            voxels[x_max, y_min, z_min],
-
-            voxels[x_min, y_max, z_max],
-            voxels[x_min, y_max, z_min],
-            voxels[x_min, y_min, z_max],
-            voxels[x_min, y_min, z_min],
-        ]).permute([1, 0]).unsqueeze(-1).contiguous()
-        sdf_val = self.trilinear_interpolate.apply(feature_stack, pts_centroid)
-        # sdf_val = trilinear_interpolation_cpu(feature_stack, pts_centroid)
-        loss = F.huber_loss(sdf_val, torch.zeros_like(sdf_val))
-
-        return loss
-
-if __name__ == "__main__":
-    roipool = vehicles_roi_pooling()
--- vehicle_reconstruction/scene_segmentation/vehicle_seg_utils.py
+++ vehicle_reconstruction/scene_segmentation/vehicle_seg_utils.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from pytorch3d import transforms
+from pytorch3d.ops.mesh_filtering import taubin_smoothing
 from pytorch3d.structures import Pointclouds
 import torch
 import torch.nn.functional as F
@@ -11,124 +12,6 @@ from cudaext.ops.roiaware_pool3d.roiaware_pool3d_utils import points_in_boxes_gp
 from cudaext.ops.roipoint_pool3d.roipoint_pool3d_utils import RoIPointPool3d
 from vehicle_reconstruction.scene_segmentation.loss_utils import shape_regular, pose_estimate_loss_batch
 from voxeltorch import TSDF, tsdf2meshes
-
-class vehicle_object_legacy:
-
-    def __init__(self, pts: torch.Tensor, bbox: torch.Tensor, k=5) -> None:
-        """
-            pts: [N, 3]
-            bbox: [7]
-        """
-        self.pts =  pts.detach().clone()
-        self.bbox = bbox.detach().clone() # [x, y, z, l, w, h, cos]
-        # self.vehicle: "vehicle" = None
-        self.vehicle_latent = torch.zeros(
-            (1, k), requires_grad=True, device=pts.device)
-        # [1, K]
-
-        self.translation = torch.tensor([
-            bbox[0].item(), bbox[1].item(), bbox[2].item(), bbox[6].item()
-        ], requires_grad=True, device=pts.device)
-
-    # def centroid(self):
-    #     pts_centroid = self.pts[:, :3] - self.translation[None, :3]
-    #     pts_centroid = torch.matmul(pts_centroid, pts_centroid.new_tensor(
-    #         [[torch.cos(-self.translation[3]),
-    #           -torch.sin(-self.translation[3]), 0.0],
-    #          [torch.sin(-self.translation[3]),
-    #           torch.cos(-self.translation[3]), 0.0],
-    #          [0.0, 0.0, 1.0],
-    #          ]
-    #     ).T)
-
-    #     # undifferentiable
-    #     bbox_centroid = torch.zeros_like(self.bbox)
-    #     bbox_centroid[3:6] = self.bbox[3:6]
-
-    #     return pts_centroid, bbox_centroid
-
-    # def reconstruct_at_scene(self, vehi_reconstructor, standard_bbox, show_rooftop: bool = False):
-    #     self.reconstruct_vehicle(vehi_reconstructor)
-    #     vehi = self.vehicle.to_mesh()
-    #     vehicle_mesh = vehi.to_trimesh()
-
-    #     vehicle_mesh.vertices *= 0.1
-    #     vehicle_mesh.vertices = vehicle_mesh.vertices[:, [2, 0, 1]]
-
-    #     vehicle_mesh.vertices -= np.array([standard_bbox[2]/2.0,
-    #                                        standard_bbox[0]/2.0,
-    #                                        self.bbox[5].cpu() / 2.0])
-    #     vehicle_mesh.vertices = np.matmul(vehicle_mesh.vertices, np.array(
-    #         [[np.cos(self.bbox[6].cpu()),
-    #           -np.sin(self.bbox[6].cpu()), 0.0],
-    #          [np.sin(self.bbox[6].cpu()),
-    #           np.cos(self.bbox[6].cpu()), 0.0],
-    #          [0.0, 0.0, 1.0],
-    #          ]
-    #     ).T)
-
-    #     vehicle_mesh.vertices += np.array([self.bbox[0].cpu(),
-    #                                        self.bbox[1].cpu(),
-    #                                        self.bbox[2].cpu()])
-
-    #     if show_rooftop:
-    #         max_y = vehicle_mesh.vertices[:, 2].max()
-    #         rooftop_idx = ((max_y - vehicle_mesh.vertices[:, 2]) < 0.2)
-    #         # rooftop_vertices = self.vertices[rooftop_idx]
-    #         # rooftop_vertices, rooftop_idx = vehi.rooftop_approximate(
-    #         #     return_idx=True)
-    #         return vehicle_mesh, rooftop_idx
-    #     else:
-    #         return vehicle_mesh
-    
-    def reconstruct_at_scene(self, vehi_reconstructor:vehicle_reconstructor, 
-                             show_rooftop: bool = False):
-        self.reconstruct_vehicle(vehi_reconstructor)
-        
-        vehi = tsdf2meshes(self.tsdf_grid, vehi_reconstructor.unit, padding=0)
-
-        centering_bbox = -torch.Tensor([vehi_reconstructor.TSDF.bbox[0], 0.0, vehi_reconstructor.TSDF.bbox[2]]).to(vehi.device) / 2
-        vehi.offset_verts_(centering_bbox) # centering the meshes
-
-        rotate_m = transforms.RotateAxisAngle(self.bbox[6], axis="Y", degrees=False)
-        vehi  = vehi.update_padded(rotate_m.transform_points(vehi.verts_padded())) # local rotation
-
-        vehi = vehi.update_padded(vehi.verts_padded()[:, :, [2, 0, 1]]) # [x, y, z] -> [z, x, y]
-        
-        centering_heights = -torch.Tensor([0.0, 0.0, self.bbox[5]]).to(vehi.device) / 2
-        vehi.offset_verts_(centering_heights) # Local -> global
-        vehi.offset_verts_(self.bbox[:3])
-        
-        return vehi
-
-        # vehicle_mesh.vertices += np.array([self.bbox[0].cpu(),
-        #                                    self.bbox[1].cpu(),
-        #                                    self.bbox[2].cpu()])
-
-        # if show_rooftop:
-        #     max_y = vehicle_mesh.vertices[:, 2].max()
-        #     rooftop_idx = ((max_y - vehicle_mesh.vertices[:, 2]) < 0.2)
-        #     # rooftop_vertices = self.vertices[rooftop_idx]
-        #     # rooftop_vertices, rooftop_idx = vehi.rooftop_approximate(
-        #     #     return_idx=True)
-        #     return vehicle_mesh, rooftop_idx
-        # else:
-        #     return vehicle_mesh
-
-    def reconstruct_vehicle(self, reconstructor: vehicle_reconstructor):
-        self.tsdf_grid = reconstructor.decode(self.vehicle_latent)
-
-    def prepare_training_data(self, reconstructor: vehicle_reconstructor, padding: int = 1):
-        self.tsdf_grid:torch.Tensor = reconstructor.decode(self.vehicle_latent)
-        voxel = voxel.permute([2, 0, 1])  # [w, h, l] -> [l, w, h]
-
-        #  padding
-        if padding > 0:
-            voxel = F.pad(voxel, (padding) * 6, "constant", 0.3)
-
-        pts_centroid, _ = self.centroid()  # [N, 3] world coordinate
-
-        return voxel, pts_centroid, self.bbox[5].item()
 
 class vehicle_object:
     def __init__(self, pts: Pointclouds, bbox: torch.Tensor, training_feature:torch.Tensor=None, k=5) -> None:
@@ -167,14 +50,15 @@ class vehicle_object:
 
     def reconstruct_at_scene(self, vehi_reconstructor:vehicle_reconstructor, 
                              threshold:int=10,
+                             smoothing:bool=True,
                              show_rooftop: bool = False):
         
         threshold_flag = (self.pts.num_points_per_cloud() > threshold)
+        packed_rooftop_idx = None
         with torch.no_grad():
-            
-
             tsdf_grid = vehi_reconstructor.decode(self.vehicle_latent)
-            vehi = tsdf2meshes(tsdf_grid, vehi_reconstructor.unit, padding=0)
+            vehi = tsdf2meshes(tsdf_grid, vehi_reconstructor.unit, padding=1) # Add padding to avoid non-watertight meshes
+            vehi.offset_verts_(-vehi_reconstructor.unit) # eliminate the offset by padding
 
             centering_bbox = -torch.Tensor([vehi_reconstructor.TSDF.bbox[0], 0.0, vehi_reconstructor.TSDF.bbox[2]]).to(vehi.device) / 2
             vehi.offset_verts_(centering_bbox) # centering the meshes
@@ -183,8 +67,20 @@ class vehicle_object:
             vehi = vehi.update_padded(self._local_world_rotate_m.transform_points(vehi.verts_padded())) # local rotation
             vehi = vehi.update_padded(self._local_world_centering_heights.transform_points(vehi.verts_padded()))
             vehi = vehi.update_padded(self._local_world_translate_m.transform_points(vehi.verts_padded()))
+            vehi = vehi[threshold_flag]
+
+            if smoothing:
+                vehi = taubin_smoothing(vehi)
+            
+            if show_rooftop:
+                # max_z = vehi.verts_padded()[:, :, 2].amax(dim = 1, keepdim=True)
+                positive_vehi = vehi.offset_verts(torch.maximum(-vehi.verts_padded().amin(dim=(0, 1)), centering_bbox.new_ones(3)))
+                max_z = positive_vehi.verts_padded()[:, :, 2].amax(dim = 1, keepdim=True)
+                packed_rooftop_idx:torch.Tensor = (max_z - positive_vehi.verts_padded()[:, :, 2]) < 0.2
+                packed_rooftop_idx = packed_rooftop_idx.view(-1)[vehi.verts_padded_to_packed_idx()]
+                
         
-        return vehi[threshold_flag]
+        return vehi, packed_rooftop_idx
     
     def get_local_point_clouds(self, pts:Pointclouds=None):
         """
@@ -203,6 +99,7 @@ class vehicle_object:
         """
         local_pts = self.get_local_point_clouds(Pointclouds(self.training_feature))
         postive_pts  = local_pts.update_padded(self._local_positive_centering.transform_points(local_pts.points_padded()))
+        postive_pts.update_padded(postive_pts.points_padded()[:, :, [1, 2, 0]]) # [z, x, y] -> [x, y, z]
         return postive_pts
     
     def get_tsdf_grid(self, vehi_reconstructor:vehicle_reconstructor):
@@ -211,13 +108,6 @@ class vehicle_object:
         """
         tsdf_grid = vehi_reconstructor.decode(self.vehicle_latent)
         return tsdf_grid
-    
-    def prepare_training_data(self, reconstructor: vehicle_reconstructor, padding: int = 1):
-        self.tsdf_grid:torch.Tensor = reconstructor.decode(self.vehicle_latent)
-        training_point_clouds = self.get_training_point_clouds()
-        
-
-        return voxel, pts_centroid, self.bbox[5].item()
     
 
 
@@ -233,19 +123,14 @@ class point_cloud_scene:
         self.vehicles: vehicle_object = None
         
         self.pose_estimate_loss_batch = pose_estimate_loss_batch()
+        self.shape_regular_fun: shape_regular = shape_regular()
 
         self.roipool3d = RoIPointPool3d(pool_extra_width = (0, 0, 0))
 
         self.segmented_pts = None # [B, N, 3]
         self.segmented_gt_boxes = None #[B, 7]
 
-        # self.estimate_loss_fun: pose_estimate_loss = pose_estimate_loss(
-        #     vehi_reconstructor.sampling_space[2],
-        #     vehi_reconstructor.sampling_space[0],
-        #     vehi_reconstructor.sampling_space[1],
-        #     vehi_reconstructor.global_res)
-        # self.shape_regular_fun: shape_regular = shape_regular(
-        #     vehi_reconstructor.S)
+
 
     
     def vehicle_seg(self):
@@ -257,18 +142,8 @@ class point_cloud_scene:
 
         for bbox_idx in range(self.gt_boxes.size(0)):
             bbox_mask = (self.pts_assign == bbox_idx)
-            
-            # if bbox_mask.sum() < point_threshold:
-            #     self.segmented_pts.append(self.pts.new_empty((0, 3)))
-            #     self.segmented_gt_boxes.append(self.gt_boxes[bbox_idx])
-            #     continue
-            
             self.segmented_pts.append(self.pts[bbox_mask, :3])
             self.segmented_gt_boxes.append(self.gt_boxes[bbox_idx])
-
-            # vehicle = vehicle_object(pts=self.pts[bbox_mask],
-            #                          bbox=self.gt_boxes[bbox_idx])
-            # self.vehicles.append(vehicle)
 
         training_feature, pooled_empty_flag = self.roipool3d.forward(self.pts[:, :3].view(1, -1, 3),
                                                             self.pts.new_empty((1, self.pts.size(1), 0)),
@@ -278,33 +153,6 @@ class point_cloud_scene:
         self.segmented_pts = Pointclouds(self.segmented_pts)
         self.segmented_gt_boxes = torch.stack(self.segmented_gt_boxes)
         self.vehicles = vehicle_object(self.segmented_pts, self.segmented_gt_boxes, training_feature)
-
-    def vehicle_seg_legacy(self, point_threshold = 10, padding_none:bool=True):
-        self.pts_assign = points_in_boxes_gpu(
-            self.pts[:, :3].view(1, -1, 3), self.gt_boxes.view(1, -1, 7)).squeeze(dim=0)
-        
-        self.segmented_pts = []
-        self.segmented_gt_boxes = []
-
-        for bbox_idx in range(self.gt_boxes.size(0)):
-            bbox_mask = (self.pts_assign == bbox_idx)
-            
-            if bbox_mask.sum() < point_threshold:
-                # FIX ME:
-                # if padding_none: 
-                #     self.vehicles.append(None)
-                continue
-            
-            self.segmented_pts.append(self.pts[bbox_mask, :3])
-            self.segmented_gt_boxes.append(self.gt_boxes[bbox_idx])
-
-            # vehicle = vehicle_object(pts=self.pts[bbox_mask],
-            #                          bbox=self.gt_boxes[bbox_idx])
-            # self.vehicles.append(vehicle)
-        
-        self.segmented_pts = Pointclouds(self.segmented_pts)
-        self.segmented_gt_boxes = torch.stack(self.segmented_gt_boxes)
-        self.vehicles = vehicle_object(self.segmented_pts, self.segmented_gt_boxes)
 
     def get_vehicles(self):
         if self.vehicles is None:
@@ -352,7 +200,10 @@ class point_cloud_scene:
             tsdf_grid = tsdf_grid[threshold_flag]
 
             estimate_loss = self.pose_estimate_loss_batch.forward(tsdf_grid, training_point_clouds.points_padded(), vehi_reconstructor.unit)
-            estimate_loss.backward()
+            shape_regulation_loss = self.shape_regular_fun.forward(
+                    self.vehicles.vehicle_latent[threshold_flag], vehi_reconstructor._S)
+            totall_loss = estimate_loss + shape_regulation_loss
+            totall_loss.backward()
 
             grad = self.vehicles.vehicle_latent.grad
             self.vehicles.vehicle_latent = self.vehicles.vehicle_latent - grad
@@ -360,39 +211,3 @@ class point_cloud_scene:
             self.vehicles.vehicle_latent.requires_grad_(True)
         end_time = time.time()
         print(f"Time cost:{end_time - start_time}, {(end_time - start_time)/iter} per epoch")
-
-
-    def pose_estimate_legacy(self, iter=5):
-        if self.vehicles is None:
-            self.vehicle_seg()
-
-        for vehi in self.vehicles:
-            if vehi is None:
-                continue
-            
-            for _ in range(iter):
-                voxel, pts_centroid, height = vehi.prepare_training_data(
-                    self.vehi_reconstructor)
-                shape_loss = self.estimate_loss_fun(
-                    voxel, pts_centroid, height)
-                shape_regulation = self.shape_regular_fun(
-                    vehi.vehicle_latent)
-
-                total_loss = (16.6 / self.vehi_reconstructor.global_res) * \
-                    shape_loss + shape_regulation
-                total_loss.backward()
-                
-                vehi.vehicle_latent = vehi.vehicle_latent - vehi.vehicle_latent.grad
-                vehi.vehicle_latent = vehi.vehicle_latent.clone().detach()
-                vehi.vehicle_latent.requires_grad_(True)
-                
-                # vehi.translation = vehi.translation - vehi.translation.grad
-                # vehi.translation = vehi.translation.clone().detach()
-                # vehi.translation.requires_grad_(True)
-
-            # if vehi.translation.grad is not None:
-            #     vehi.translation = vehi.translation - vehi.translation.grad
-            #     vehi.translation = vehi.translation.clone().detach()
-            #     vehi.translation.requires_grad_(True)
-            # print(vehi.vehicle_latent.grad)
-            # print(vehi.translation.grad)
